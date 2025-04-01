1e41ce18e12ed16cd87b825be9cbf110add43225
User <jleo101007.2@gmail.com>
2025-04-01 17:05:08 +0300

    First ver

---
+++ app/src/main/java/com/example/tictactoe2players/GameActivity.java
@@ -0,0 +1,17 @@
+package com.example.tictactoe2players;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_game);
+        BoardGame boardGame = new BoardGame(this); // canvas
+        setContentView(boardGame);
+
+    }
+}
