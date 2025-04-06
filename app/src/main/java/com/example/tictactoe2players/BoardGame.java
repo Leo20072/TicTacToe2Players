8214f6f08106d50d969b51d10b58732bf803d996
User <jleo101007.2@gmail.com>
2025-04-06 17:27:40 +0300

    First ver + Firebase

--- app/src/main/java/com/example/tictactoe2players/BoardGame.java
+++ app/src/main/java/com/example/tictactoe2players/BoardGame.java
@@ -3,13 +3,17 @@ package com.example.tictactoe2players;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 
 public class BoardGame extends View {
+
+    private Context context;
     public BoardGame(Context context) {
         super(context);
+        this.context = context;
     }
 
     @Override
@@ -18,5 +22,20 @@ public class BoardGame extends View {
 
         canvas.drawCircle(500,500,200,new Paint());
 
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            float x = event.getX();
+            float y = event.getY();
+
+            ((GameActivity)context).setPositionToFirebase(0,0);
+
+        }
+
+
+        return false;
     }
 }
--- app/src/main/java/com/example/tictactoe2players/GameActivity.java
+++ app/src/main/java/com/example/tictactoe2players/GameActivity.java
@@ -3,15 +3,29 @@ package com.example.tictactoe2players;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
 
+    private FbModule fbModule;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        fbModule = new FbModule(this);
+
+
         //setContentView(R.layout.activity_game);
         BoardGame boardGame = new BoardGame(this); // canvas
         setContentView(boardGame);
 
+    }
+
+    public void setPositionToFirebase(int line, int col) {
+        fbModule.setPositionToFirebase(new Position(line,col));
+    }
+
+    public void setPositionFromFb(Position position) {
+        Toast.makeText(this, "" + position.getCol(), Toast.LENGTH_SHORT).show();
     }
 }
