c7a4f7492250b0bed5b91b45d2422d49d65332aa
User <jleo101007.2@gmail.com>
2025-04-20 16:28:32 +0300

    second ver

--- app/src/main/java/com/example/tictactoe2players/BoardGame.java
+++ app/src/main/java/com/example/tictactoe2players/BoardGame.java
@@ -20,7 +20,7 @@ public class BoardGame extends View {
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawCircle(500,500,200,new Paint());
+
 
     }
 
